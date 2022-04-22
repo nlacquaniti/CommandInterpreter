d0b6d5cadd57022e2bcd53a5f2c2180e8022e1da
Nikolas <nikolas.lacquaniti@gmail.com>
2022-04-22 14:25:19 +0200

    project setup

---
+++ sanity_tests/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int, char**)
+{
+	// This executable links against your library.
+	// You may perform any sanity tests here that you wish,
+	// to verify that your code works as expected.
+	// Please leave these tests in your final solution,
+	// as they will add to your overall evaluation.
+
+	return 0;
+}
---
+++ src/Control.cpp
@@ -0,0 +1,16 @@
+#include "CommandInterpreter/Control.h"
+
+void CmdInt_Initialise()
+{
+	// Initialise your resources here.
+}
+
+void CmdInt_ShutDown()
+{
+	// Clean up your resources here.
+}
+
+void CmdInt_Poll()
+{
+	// Perform any processing here.
+}
